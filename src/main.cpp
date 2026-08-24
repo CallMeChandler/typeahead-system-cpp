bef1c2754a01c9a6d91ec229ab95e3b107fb5035
CallMeChandler <aakarsh0324@gmail.com>
2026-08-25 03:19:06 +0530

    Exposed trie autocomplete through search API

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,61 @@
-// #include "crow.h"
-
-// int main(){
-//     crow::SimpleApp app;
-
-//     CROW_ROUTE(app, "/health")
-//     ([] {
-//         return crow::response{
-//             200,
-//             R"({"status":"ok"})"
-//         };
-//     });
-
-//     app.port(18080).multithreaded().run();
-
-//     return 0;
-// }
-
 #include <iostream>
+
+#include "crow.h"
 #include "trie/Trie.hpp"
 
-int main() {
+int main(){
+    crow::SimpleApp app;
+
     Trie trie;
 
-    for(int i=0;i<5;i++) trie.insert("car");
-    for(int i=0;i<3;i++) trie.insert("camera");
-    for(int i=0;i<2;i++) trie.insert("cat");
+    trie.insert("car");
+    trie.insert("car");
+    trie.insert("car");
+    trie.insert("camera");
+    trie.insert("camera");
+    trie.insert("cat");
     trie.insert("camp");
 
-    auto suggestions = trie.startsWith("ca");
+    CROW_ROUTE(app, "/health")
+    ([]{
+        return crow::response(
+            200,
+            R"({"status":"ok"})"
+        );
+    });
 
-    for (const auto& s : suggestions) {
-        std::cout << s.word << " : " << s.frequency << '\n';
-    }
+    CROW_ROUTE(app, "/search")
+    ([&trie](const crow::request& req){
+        const auto& queryParams = req.url_params;
+        
+        const char* query = queryParams.get("q");
+
+        if (query==nullptr||query[0]=='\0'){
+            return crow::response(
+                400,
+                R"({"error":"missing query parameter"})"
+            );
+        }
+
+        auto suggestions = trie.startsWith(query);
+
+        crow::json::wvalue response;
+
+        size_t index = 0;
+
+        for (const auto& suggestion : suggestions) {
+            crow::json::wvalue item;
+
+            item["word"] = suggestion.word;
+            item["frequency"] = suggestion.frequency;
+
+            response["suggestions"][index++] = std::move(item);
+        }
+
+        return crow::response(response);
+    });
+
+    app.port(18080).multithreaded().run();
+
+    return 0;
 }
